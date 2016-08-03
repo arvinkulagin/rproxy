421079a30ed7d0844a6552ba3a7e2a32954bdaa9
Arvin Kulagin <arvinkulagin@yandex.ru>
2016-08-03 09:32:05 +0300

    Add http tests

--- transport/regexp_test.go
+++ transport/regexp_test.go
@@ -2,27 +2,34 @@ package transport
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"net/http/httputil"
 	"net/url"
 	"regexp"
+	"strings"
 	"testing"
 )
 
-func TestRegexpTransport(t *testing.T) {
+func TestRegexpTransportText(t *testing.T) {
+	// Testing backend server, that will be target for reverse proxy.
 	target := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Lorem ipsum dolor set amet")
 	}))
 	defer target.Close()
 
+	// Regular expression for replacement substrings in target server response.
 	re := regexp.MustCompile("[I,i]psum")
+
+	//
 	targetURL, err := url.Parse(target.URL)
 	if err != nil {
 		t.Error(err)
 	}
+
+	// Handler for proxy server with custom transport.
 	handler := httputil.NewSingleHostReverseProxy(targetURL)
 	handler.Transport = NewRegexpTransport(re, "shmipsum")
 	proxy := httptest.NewServer(handler)
@@ -34,19 +41,94 @@ func TestRegexpTransport(t *testing.T) {
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("response status is not OK: %s", resp.Status)
-	}
-
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		t.Error(err)
 	}
-
 	if string(body) != "Lorem shmipsum dolor set amet\n" {
 		t.Errorf("wrong response body: %s", string(body))
 	}
+	if resp.ContentLength != 30 {
+		t.Errorf("wrong response content length: %d", resp.ContentLength)
+	}
+}
 
+func TestRegexpTransportJSON(t *testing.T) {
+	// Testing backend server, that will be target for reverse proxy.
+	target := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(w, `{"text":"Lorem ipsum dolor set amet"}`)
+	}))
+	defer target.Close()
+
+	// Regular expression for replacement substrings in target server response.
+	re := regexp.MustCompile("[I,i]psum")
+
+	//
+	targetURL, err := url.Parse(target.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Handler for proxy server with custom transport.
+	handler := httputil.NewSingleHostReverseProxy(targetURL)
+	handler.Transport = NewRegexpTransport(re, "shmipsum")
+	proxy := httptest.NewServer(handler)
+	defer proxy.Close()
+
+	resp, err := http.Get(proxy.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Error(err)
+	}
+	if string(body) != `{"text":"Lorem ipsum dolor set amet"}`+"\n" {
+		t.Errorf("wrong response body: %s", string(body))
+	}
+	if resp.ContentLength != 38 {
+		t.Errorf("wrong response content length: %d", resp.ContentLength)
+	}
+}
+
+func TestRegexpTransportPost(t *testing.T) {
+	// Testing backend server, that will be target for reverse proxy.
+	target := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.Copy(w, r.Body)
+	}))
+	defer target.Close()
+
+	// Regular expression for replacement substrings in target server response.
+	re := regexp.MustCompile("[I,i]psum")
+
+	//
+	targetURL, err := url.Parse(target.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Handler for proxy server with custom transport.
+	handler := httputil.NewSingleHostReverseProxy(targetURL)
+	handler.Transport = NewRegexpTransport(re, "shmipsum")
+	proxy := httptest.NewServer(handler)
+	defer proxy.Close()
+
+	resp, err := http.Post(proxy.URL, "text/plain", strings.NewReader("Lorem ipsum dolor set amet\n"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Error(err)
+	}
+	if string(body) != "Lorem shmipsum dolor set amet\n" {
+		t.Errorf("wrong response body: %s", string(body))
+	}
 	if resp.ContentLength != 30 {
 		t.Errorf("wrong response content length: %d", resp.ContentLength)
 	}
